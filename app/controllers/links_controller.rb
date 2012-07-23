b960a83059d510ba19479bba7119a88c20335ca2
Dom Kiva-Meyer <git@domkm.com>
2012-07-23 15:56:15 -0700

    finishes interation 2

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,14 +1,19 @@
 class LinksController < ApplicationController
   def index
+    @links = Link.all
   end
 
   def show
+    @link = Link.find(params[:id])
   end
 
   def new
+    @link = Link.new
   end
 
   def create
+    @link = Link.create(params[:link])
+    redirect_to link_path(@link)
   end
 
   def edit
