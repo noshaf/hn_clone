adb5ba5d3756db6711e2df0b78a6cd9f740fea11
Dom Kiva-Meyer <git@domkm.com>
2012-07-23 22:03:43 -0700

    generates users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 HnClone::Application.routes.draw do
 
+  get "users/index"
+
+  get "users/show"
+
+  get "users/new"
+
+  get "users/create"
+
+  get "users/edit"
+
+  get "users/update"
+
+  get "users/destroy"
+
   devise_for :users
 
   root to: "links#index"
