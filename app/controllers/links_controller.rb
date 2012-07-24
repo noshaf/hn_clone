3756d15d6102ce464af25bd01043a8aaa398dcec
Apprentice <apprentice@devbootcamp.com>
2012-07-24 12:19:25 -0700

    iteration 3

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -15,9 +15,11 @@ class LinksController < ApplicationController
 
   def create
     @link = Link.new(params[:link])
-    @link.clean_url
-    @link.save!
-    redirect_to link_path(@link)
+    if @link.save
+      redirect_to link_path(@link)
+    else
+      render 'new'
+    end
   end
 
   def edit
